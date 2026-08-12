de29edb9f3d006119eb2fac5b1337fc4b998fd04
Shyam <shyam666fg@gmail.com>
2026-08-12 13:58:05 +0530

    chore: initialize StrideMate backend foundation

---
+++ backend/src/main/java/com/stridemate/api/StrideMateApiApplication.java
@@ -0,0 +1,13 @@
+package com.stridemate.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrideMateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrideMateApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/stridemate/api/StrideMateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stridemate.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrideMateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
